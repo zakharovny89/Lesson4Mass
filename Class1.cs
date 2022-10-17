043ec49cfcd55b43db12c14c7e4a1c548dfaae0a
zakharovny89 <zakharovny89@gmail.com>
2022-10-17 23:24:55 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4Mass
+{
+    internal class Counter
+    {
+        internal static void NumberOfPositive(int[,] Massive, int row, int column)
+        {
+            int numberOfPositive = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (Massive[i, j] >0)
+                    {
+                        numberOfPositive++;
+                    }
+                }
+            }
+            Console.WriteLine(numberOfPositive);
+        }
+        internal static void NumberOfNegative(int[,] Massive, int row, int column)
+        {
+            int numberOfNegative = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (Massive[i, j] < 0)
+                    {
+                        numberOfNegative++;
+                    }
+                }
+            }
+
+            Console.WriteLine(numberOfNegative);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+using Lesson4Mass;
+
+int row;
+int column;
+bool end = false;
+
+Console.WriteLine("How many rows are in the matrix?");
+row = int.Parse(Console.ReadLine());
+Console.WriteLine("How many column are in the matrix?");
+column = int.Parse(Console.ReadLine());
+
+int[,] Massive = new int[row, column];
+
+for (int i=0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        Console.WriteLine($"Enter parameter value with index {i} : {j}");
+        Massive[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Your array looks like this:");
+
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        Console.Write($"{Massive[i, j],10}");        
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
+
+while (!end)
+{
+    Console.WriteLine("If you want to count the number of positive numbers, click '+'\r\n" +
+        "If you want to count the number of negative numbers, click '-'\r\n" +
+        "If you want to Exit, press any other key");
+    string WhatsNext = Console.ReadLine();
+
+    switch (WhatsNext)
+    {
+        case "+":
+            Counter.NumberOfPositive(Massive, row, column);
+            break;
+        case "-":
+            Counter.NumberOfNegative(Massive, row, column);
+            break;
+        default:
+            end = true;
+            return;
+    }
+}
